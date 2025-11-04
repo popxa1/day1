929a6e4bca78aea695d236bd7346adf00aa7a292
mpopxadze95 <mpopxadze95@gmail.com>
2025-11-04 22:30:23 +0400

    pirveli dge

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What city were you born in?\n")
+pet = input("what is your pets name?\n")
+print("Your band name could be:" + city + " " + pet)
